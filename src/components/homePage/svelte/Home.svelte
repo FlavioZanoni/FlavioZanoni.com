24a22ce143df8b116aa26a597dedeca1b1302eba
FlavioZanoni <flaviozg888@gmail.com>
2024-04-01 11:50:44 -0300

    styling

--- src/components/homePage/svelte/Home.svelte
+++ src/components/homePage/svelte/Home.svelte
@@ -1,54 +1,63 @@
 <script lang="ts">
-import { state } from "@homePage/svelte/store"
-import type { HomeGridItem } from "@lib/state/types"
-import { onMount } from "svelte"
-const gridColumns = 16
-const gridRows = 9
+  import { state } from "@homePage/svelte/store"
+  import type { HomeGridItem } from "@lib/state/types"
+  import { onMount } from "svelte"
+  const gridColumns = 16
+  const gridRows = 9
 
-// create full grid
-const grid: HomeGridItem[] = Array.from(
-  { length: gridColumns * gridRows },
-  (_, i) => {
-    const x = i % gridColumns
-    const y = Math.floor(i / gridColumns)
+  const cellWidth = window.innerWidth / gridColumns
+  const cellHeight = (window.innerHeight - 40) / gridRows
 
-    return {
-      pos: { x, y },
-      icon: "",
-      title: "",
-      action: "",
+  // create full grid
+  const grid: HomeGridItem[] = Array.from(
+    { length: gridColumns * gridRows },
+    (_, i) => {
+      const x = i % gridColumns
+      const y = Math.floor(i / gridColumns)
+
+      return {
+        pos: { x, y },
+        icon: "",
+        title: "",
+        action: "",
+      }
     }
-  }
-)
-
-// populate grid with state
-grid.forEach((cell, index) => {
-  const cellState = $state.homeGrid.items.find(
-    (s) => s.pos.x === cell.pos.x && s.pos.y === cell.pos.y
   )
 
-  if (cellState) {
-    grid[index] = cellState
-  }
-})
+  // populate grid with state
+  grid.forEach((cell, index) => {
+    const cellState = $state.homeGrid.items.find(
+      (s) => s.pos.x === cell.pos.x && s.pos.y === cell.pos.y
+    )
 
-onMount(() => {
-  console.log("mounted", $state)
-})
+    if (cellState) {
+      grid[index] = cellState
+    }
+  })
 
+  onMount(() => {
+    console.log("mounted", $state)
+  })
 </script>
 
-<div class="grid w-full h-full"
-    style={
-        `grid-template-columns: repeat(${gridColumns}, 1fr);
-        grid-template-rows: repeat(${gridRows}, 1fr);`
-    }
+<div
+  class="grid"
+  style={`grid-template-columns: repeat(${gridColumns}, 1fr);
+        grid-template-rows: repeat(${gridRows}, 1fr);
+        `}
 >
-    {#each grid as cell}
-        <div class="flex flex-col gap-2 w-full h-full">
-            <img src={cell.icon} alt={cell.title} />
-            <p>{cell.title}</p>
-            {cell.pos.x} {cell.pos.y}
-        </div>
-    {/each}
+  {#each grid as cell}
+    <div
+      class={`flex flex-col gap-2`}
+      style={`
+          width: ${cellWidth}px;
+          height: ${cellHeight}px;
+          `}
+    >
+      <img src={cell.icon} alt={cell.title} />
+      <p>{cell.title}</p>
+      {cell.pos.x}
+      {cell.pos.y}
+    </div>
+  {/each}
 </div>
