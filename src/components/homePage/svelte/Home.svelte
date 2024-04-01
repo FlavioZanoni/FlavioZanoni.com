5525889af7ad12ea73bf9b5abfe61137d738f6e4
FlavioZanoni <flaviozg888@gmail.com>
2024-04-01 15:50:51 -0300

    adding drag and drop

--- src/components/homePage/svelte/Home.svelte
+++ src/components/homePage/svelte/Home.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
   import { state } from "@homePage/svelte/store"
   import type { HomeGridItem } from "@lib/state/types"
-  import { onMount } from "svelte"
+
   const gridColumns = 16
   const gridRows = 9
-
   const cellWidth = window.innerWidth / gridColumns
   const cellHeight = (window.innerHeight - 40) / gridRows
 
-  // create full grid
-  const grid: HomeGridItem[] = Array.from(
+  // populate grid items
+  let gridItems: HomeGridItem[] = Array.from(
     { length: gridColumns * gridRows },
     (_, i) => {
       const x = i % gridColumns
@@ -24,20 +23,43 @@
     }
   )
 
-  // populate grid with state
-  grid.forEach((cell, index) => {
-    const cellState = $state.homeGrid.items.find(
-      (s) => s.pos.x === cell.pos.x && s.pos.y === cell.pos.y
-    )
+  let grid = []
 
-    if (cellState) {
-      grid[index] = cellState
-    }
-  })
+  $: {
+    // populate grid with state
+    grid = [
+      ...gridItems.map((cell) => {
+        const cellState = $state.homeGrid.items.find(
+          (s) => s.pos.x === cell.pos.x && s.pos.y === cell.pos.y
+        )
 
-  onMount(() => {
-    console.log("mounted", $state)
-  })
+        return cellState ? cellState : cell
+      }),
+    ]
+  }
+
+  const onDrop = (e: DragEvent, current: HomeGridItem) => {
+    e.preventDefault()
+    const data = JSON.parse(e.dataTransfer.getData("text/plain"))
+
+    state.update((prev) => {
+      const itemToUpdate = prev.homeGrid.items.find(
+        (i) => i.pos.x === data.pos.x && i.pos.y === data.pos.y
+      )
+
+      if (itemToUpdate) {
+        itemToUpdate.pos = current.pos
+      }
+
+      const prevCopy = { ...prev }
+
+      prevCopy.homeGrid.items = prev.homeGrid.items.map((item) =>
+        item === itemToUpdate ? itemToUpdate : item
+      )
+
+      return prevCopy
+    })
+  }
 </script>
 
 <div
@@ -48,16 +70,26 @@
 >
   {#each grid as cell}
     <div
-      class={`flex flex-col gap-2`}
+      role="button"
+      tabindex="0"
+      class={`flex gap-2 items-center justify-center p-2`}
       style={`
           width: ${cellWidth}px;
           height: ${cellHeight}px;
           `}
+      draggable={cell.title !== ""}
+      on:dragstart={(e) => {
+        e.dataTransfer.setData("text/plain", JSON.stringify(cell))
+      }}
+      on:drop={(e) => onDrop(e, cell)}
+      on:dragover={(e) => {
+        e.preventDefault()
+      }}
     >
-      <img src={cell.icon} alt={cell.title} />
-      <p>{cell.title}</p>
-      {cell.pos.x}
-      {cell.pos.y}
+      <div class="w-full h-full flex-col flex items-center justify-center">
+        <img src={cell.icon} alt={cell.title} />
+        <p>{cell.title}</p>
+      </div>
     </div>
   {/each}
 </div>
