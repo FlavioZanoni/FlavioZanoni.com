05a4ed59d5e8c1220f1896e77ed0391eb7c0a2b6
FlavioZanoni <flaviozg888@gmail.com>
2024-04-10 20:32:12 -0300

    jpg to webp and new images
    also changing aspect ratio and bigger color picker buttons

--- src/components/homePage/svelte/applications/Background.svelte
+++ src/components/homePage/svelte/applications/Background.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import { desktopStore } from "@lib/store"
 
-  // You can handle the logic here
-
   const handleColor = (color: string) => {
     console.log("Color clicked", color)
 
@@ -62,72 +60,85 @@
 
   <div class="flex flex-col gap-4">
     <div class="flex flex-col gap-2">
-      <h2 class="text-xl font-mediumw-full">Solid colors</h2>
+      <h2 class="text-xl font-mediumw-full">Solid colors:</h2>
       <div class="flex flex-wrap gap-4">
         <button
           on:click={() => handleColor("#ef4444")}
-          class="w-8 h-8 bg-red-500"
+          class="w-10 h-10 bg-red-500"
         ></button>
         <button
           on:click={() => handleColor("#22c55e")}
-          class="w-8 h-8 bg-green-500"
+          class="w-10 h-10 bg-green-500"
         ></button>
         <button
           on:click={() => handleColor("#3b82f6")}
-          class="w-8 h-8 bg-blue-500"
+          class="w-10 h-10 bg-blue-500"
         ></button>
         <button
           on:click={() => handleColor("#eab308")}
-          class="w-8 h-8 bg-yellow-500"
+          class="w-10 h-10 bg-yellow-500"
         ></button>
         <button
           on:click={() => handleColor("#ec4899")}
-          class="w-8 h-8 bg-pink-500"
+          class="w-10 h-10 bg-pink-500"
         ></button>
         <button
           on:click={() => handleColor("#a855f7")}
-          class="w-8 h-8 bg-purple-500"
+          class="w-10 h-10 bg-purple-500"
         ></button>
         <button
           on:click={() => handleColor("#6366f1")}
-          class="w-8 h-8 bg-indigo-500"
+          class="w-10 h-10 bg-indigo-500"
         ></button>
         <button
           on:click={() => handleColor("#06b6d4")}
-          class="w-8 h-8 bg-cyan-500"
+          class="w-10 h-10 bg-cyan-500"
         ></button>
         <button
           on:click={() => handleColor("#6b7280")}
-          class="w-8 h-8 bg-gray-500"
+          class="w-10 h-10 bg-gray-500"
         ></button>
         <button
           on:click={() => handleColor("#64748b")}
-          class="w-8 h-8 bg-slate-500"
+          class="w-10 h-10 bg-slate-500"
         ></button>
         <button
           on:click={() => handleColor("#ffffff")}
-          class="w-8 h-8 border border-slate-200 bg-white"
+          class="w-10 h-10 border border-slate-200 bg-white"
         ></button>
-        <button on:click={() => handleColor("#000000")} class="w-8 h-8 bg-black"
+        <button
+          on:click={() => handleColor("#000000")}
+          class="w-10 h-10 bg-black"
         ></button>
       </div>
     </div>
 
     <div class="flex flex-col gap-2">
-      <h2 class="text-xl font-medium">Images</h2>
+      <h2 class="text-xl font-medium">Images:</h2>
       <div class="flex flex-wrap gap-4">
-        <button on:click={() => handleDefaultImages("xp.jpg")}>
+        <button on:click={() => handleDefaultImages("Bliss.webp")}>
           <img
-            src="backgrounds/xp.jpg"
-            class="w-24 h-24 bg-cover bg-center"
-            alt="win xp background"
+            src="backgrounds/Bliss.webp"
+            class="w-28 h-16 bg-cover bg-center"
+            alt="Bliss background"
           />
+          <p>Bliss</p>
         </button>
-        <button>
-          <img class="w-24 h-24 bg-cover bg-center" />
+        <button on:click={() => handleDefaultImages("Autumn.webp")}>
+          <img
+            src="backgrounds/Autumn.webp"
+            class="w-28 h-16 bg-cover bg-center"
+            alt="Autumn background"
+          />
+          <p>Autumn</p>
         </button>
-        <button>
-          <img class="w-24 h-24 bg-cover bg-center" />
+        <button on:click={() => handleDefaultImages("Tsumugi.webp")}>
+          <img
+            src="backgrounds/Tsumugi.webp"
+            class="w-28 h-16 bg-cover bg-center"
+            alt="Kotobuki Tsumugi from K-ON! as a background"
+          />
+          <p>Tsumugi</p>
         </button>
       </div>
     </div>
@@ -140,26 +151,22 @@
 
     <div class="flex flex-col gap-4">
       <h2 class="text-xl font-semibold">Upload a custom image</h2>
-      <input
-        on:change={handleCustomImage}
-        type="file"
-        accept="image/*"
-        class="mt-2"
-      />
+      <input on:change={handleCustomImage} type="file" accept="image/*" />
     </div>
 
     <div>
       {#if $desktopStore.background.userImages.length > 0}
         <div class="flex flex-col gap-2">
-          <h2 class="text-xl font-medium">Your images</h2>
+          <h2 class="text-xl font-medium">Your images:</h2>
           <div class="flex flex-wrap gap-4">
             {#each $desktopStore.background.userImages as image}
               <button on:click={() => handleInMemoryImage(image.base64)}>
                 <img
                   src={image.base64}
-                  class="w-24 h-24 bg-cover bg-center"
+                  class="w-28 bg-cover bg-center"
                   alt={image.fileName}
                 />
+                <p>{image.fileName.split(".")[0]}</p>
               </button>
             {/each}
           </div>
