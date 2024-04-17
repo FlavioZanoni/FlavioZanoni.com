d43c3eaec6ab1453198b538ae576fa0d185d8874
FlavioZanoni <flaviozg888@gmail.com>
2024-04-17 18:26:11 -0300

    keeping track of lines
    the way i did before, relies on the buffersize, witch stays the same as you scroll

--- src/lib/terminal/index.ts
+++ src/lib/terminal/index.ts
@@ -16,6 +16,8 @@ const availabeleCommands = [
 export class Term {
   constructor(public term: TerminalType) {}
 
+  private wrotedLines = 0
+
   private playBeep() {
     const context = new AudioContext()
     const oscillator = context.createOscillator()
@@ -32,6 +34,12 @@ export class Term {
     oscillator.stop(context.currentTime + 0.1)
   }
 
+  private getDecorationString() {
+    const currentPwd = pwd()
+    const emoji = currentPwd === "root" ? "⌂" : "☺"
+    return `\x1B[34m${emoji} ${currentPwd}\x1B[32m ❯\x1B[0m `
+  }
+
   public setup(dir: string) {
     // need to set the PWD to the directory sent
 
@@ -40,35 +48,32 @@ export class Term {
 
     this.term.writeln("Welcome to the terminal!")
     this.term.writeln("Type 'help' to get started.")
+    this.wrotedLines = 2
 
     this.term.options.cursorInactiveStyle = "block"
     this.term.options.cursorBlink = true
     this.term.options.fontFamily = "IBM"
     this.term.options.lineHeight = 1.4
-    this.term.write(`\x1B[34m⌂ ${pwd()}\x1B[32m ❯\x1B[0m `)
+    this.term.write(this.getDecorationString())
 
     this.term.onBell(() => {
       this.playBeep()
     })
 
     this.term.onData((data) => {
-      console.log(data)
-
       switch (data) {
         case "\r":
-          const lastLine = this.term.buffer.active.getLine(
-            this.term.buffer.active.cursorY
-          )
+          const lastLine = this.term.buffer.active.getLine(this.wrotedLines)
           const lastLineText = lastLine.translateToString().trim()
-          if (lastLineText === "") {
-            this.term.writeln("")
+          if (lastLineText.length === pwd().length + 4) {
+            this.writeln("")
             break
           }
           this.execCommand(lastLineText)
           break
         case "\x7f":
+          // pwd + 5 is the length of the prompt decoration
           if (this.term.buffer.active.cursorX === pwd().length + 5) {
-            // pwd + 5 is the length of the prompt decoration
             this.term.write("\x07") // trigger bell
             break
           }
@@ -80,12 +85,6 @@ export class Term {
           this.term.write("")
           break
         case "\x1b[B":
-          this.term.write("")
-          break
-        case "\x1b[C":
-          this.term.write("")
-          break
-        case "\x1b[D":
           this.term.write("")
           break
 
@@ -100,24 +99,28 @@ export class Term {
   }
 
   public writeln(str: string) {
-    const currentPwd = pwd()
-    const emoji = currentPwd === "root" ? "⌂" : "☺"
-
-    this.term.write(`\x1B[34m${emoji} ${pwd()}\x1B[32m ❯\x1B[0m `)
     this.term.writeln(str)
-    this.term.write(`\x1B[34m${emoji} ${pwd()}\x1B[32m ❯\x1B[0m `)
+    this.term.write(this.getDecorationString())
+    this.wrotedLines++
   }
 
   public execCommand(str: string) {
-    const [start, typed] = str.split("❯").map((str) => str.trim())
-    const [command, ...args] = typed.split(" ").map((arg) => arg.trim())
+    const [_, typed] = str.split("❯")
+    const [command, ...args] = typed
+      .trim()
+      .split(" ")
+      .map((arg) => arg.trim())
+
     this.term.writeln("")
+    this.wrotedLines++
 
     switch (command as (typeof availabeleCommands)[number]) {
       case "echo":
         return this.writeln(args.join(" "))
       case "clear":
-        return this.term.clear()
+        this.term.clear()
+        this.wrotedLines = 1
+        return this.term.write(this.getDecorationString())
       case "pwd":
         const curr = pwd()
         return this.writeln(curr)
@@ -127,7 +130,7 @@ export class Term {
       case "touch":
         try {
           touch(args[0])
-          this.writeln("")
+          this.term.write(this.getDecorationString())
         } catch (e) {
           return this.writeln(`touch: ${e.message || "Error"}`)
         }
@@ -135,7 +138,7 @@ export class Term {
       case "mkdir":
         try {
           mkdir(args[0])
-          this.writeln("")
+          this.term.write(this.getDecorationString())
         } catch (e) {
           return this.writeln(`mkdir: ${e.message || "Error"}`)
         }
@@ -143,7 +146,7 @@ export class Term {
       case "cd":
         try {
           cd(args[0])
-          this.writeln("")
+          this.term.write(this.getDecorationString())
         } catch (e) {
           return this.writeln(`cd: ${e.message || "Error"}`)
         }
