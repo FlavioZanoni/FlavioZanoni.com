fde196f8fe2e85236afd00e34e52121508f79532
FlavioZanoni <flaviozg888@gmail.com>
2024-04-22 20:28:27 -0300

    starting on resume with mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import svelte from "@astrojs/svelte"
-import tailwind from "@astrojs/tailwind"
-import { enhancedImages } from "@sveltejs/enhanced-img"
-import { defineConfig } from "astro/config"
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import { enhancedImages } from "@sveltejs/enhanced-img";
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
   vite: {
-    plugins: [enhancedImages()],
-  },
-})
+    plugins: [enhancedImages()]
+  }
+});
