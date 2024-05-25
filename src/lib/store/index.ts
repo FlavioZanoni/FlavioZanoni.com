d8eb94805da1066965d81bf5428bf509d3671c28
FlavioZanoni <flaviozg888@gmail.com>
2024-05-24 23:25:19 -0300

    handling empty icon

--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -5,12 +5,13 @@ import type { OSStore } from "./types"
 export const osStore = writable<OSStore>(jsonState as unknown as OSStore)
 
 osStore.subscribe((value) => {
-  if (
-    value.fileSystem.iNodes["10"].blocks.length &&
-    value.fileSystem.disk.apps["recycleBin"].icon === "trash.png"
-  ) {
-    value.fileSystem.disk.apps["recycleBin"].icon = "trash-full.png"
-  }
-
   console.log(value)
+
+  if (value.fileSystem.iNodes["10"].blocks.length) {
+    if (value.fileSystem.disk.apps["recycleBin"].icon !== "trash.png") return
+
+    value.fileSystem.disk.apps["recycleBin"].icon = "trash-full.png"
+    return
+  }
+  value.fileSystem.disk.apps["recycleBin"].icon = "trash.png"
 })
